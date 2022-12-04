1fd14369a36daae5e73257c416e4af5773a3d73b
mg <mathieu.george92@gmail.com>
2022-12-04 11:02:15 +0100

    day 4 part 2 partial overlap

--- src/aoc_day4/Day4_Part2.java
+++ src/aoc_day4/Day4_Part2.java
@@ -6,24 +6,25 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
-public class Day4 {
+public class Day4_Part2 {
 
     /**
      * Takes 2 strings following the format xx-xx (xx being numbers).
-     * Determines if one of the xx-xx completely overlaps the other.
-     * (ex : 2-8 overlaps 3-7)
+     * Determines if one of the xx-xx partially overlaps the other.
+     * (ex : 2-5 overlaps with 4-7)
+     * IMPORTANT : assumes that the first part of the assignment is inferior to the second.
      * @param s1 format xx-xx
      * @param s2 format xx-xx
      * @return true if an assignment overlaps the other
      */
-    static boolean isFullOverlap(String s1, String s2) {
+    static boolean isPartialOverlap(String s1, String s2) {
         Integer[] firstAssignment = parseAssignment(s1);
         Integer[] secondAssignment = parseAssignment(s2);
 
-        final boolean firstOverlapsSecond = firstAssignment[0] <= secondAssignment[0]
-                && firstAssignment[1] >= secondAssignment[1];
-        final boolean secondOverlapsFirst = secondAssignment[0] <= firstAssignment[0]
-                && secondAssignment[1] >= firstAssignment[1];
+        final boolean firstOverlapsSecond = firstAssignment[1] >= secondAssignment[0]
+                && firstAssignment[0] <= secondAssignment[1];
+        final boolean secondOverlapsFirst = secondAssignment[1] >= firstAssignment[0]
+                && secondAssignment[0] <= firstAssignment[1];
 
         return firstOverlapsSecond || secondOverlapsFirst;
     }
@@ -47,7 +48,7 @@ public class Day4 {
         int compteur = 0;
         for (String pair : pairs) {
             String[] assignments = pair.split(",");
-            if (isFullOverlap(assignments[0], assignments[1])) {
+            if (isPartialOverlap(assignments[0], assignments[1])) {
                 compteur++;
             }
         }
