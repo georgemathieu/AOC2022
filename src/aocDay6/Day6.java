7b3d84843055703d6486057e65edca14c7927398
Mathieu George <mathieu.george@koeos.lan>
2022-12-06 10:04:22 +0100

    day 6 part 2 14 chars

--- src/aocDay6/Day6.java
+++ src/aocDay6/Day6.java
@@ -4,17 +4,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Day6 {
 	
 	static int findFirstNonRepeatedCharacter(String input) {
-		for (int i = 3; i < input.length(); i++) {
-			char ref = input.charAt(i);
-			char ref1 = input.charAt(i - 1);
-			char ref2 = input.charAt(i - 2);
-			char ref3 = input.charAt(i - 3);
-			if (ref != ref1 && ref != ref2 && ref != ref3
-					&& ref1 != ref2 && ref1 != ref3 && ref2 != ref3) {
+		Set<Character> chars = new HashSet<>();
+		for (int i = 13; i < input.length(); i++) {
+			 chars.clear();
+			for (int j = i; j > i - 14; j-- ) {
+				chars.add(input.charAt(j));
+			}
+			
+			if (chars.size() == 14) {
 				return i + 1;
 			}
 		}
