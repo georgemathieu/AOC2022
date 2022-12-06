41fef178d8a8f3cb27a9e80bd91816907baf8ff5
Mathieu George <mathieu.george@koeos.lan>
2022-12-06 10:48:19 +0100

    day 7 preparation

---
+++ src/aocDay7/Day7.java
@@ -0,0 +1,15 @@
+package aocDay7;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Day7 {
+	
+	public static void main(String[] args) throws IOException {
+		Path inputPath = Paths.get("src/aocDay7/input_day7.txt");
+        String input = Files.readString(inputPath);
+	}
+
+}
