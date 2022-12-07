fb0b71a6ddd28094c51bac0805c93a3a3e97a802
mg <mathieu.george92@gmail.com>
2022-12-07 18:34:13 +0100

    day 8 init

---
+++ src/aocDay8/Day8.java
@@ -0,0 +1,15 @@
+package aocDay8;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Day8 {
+    
+    public static void main(String[] args) throws IOException {
+        Path inputPath = Paths.get("src/aocDay8/input_day8.txt");
+        String input = Files.readString(inputPath);
+    }
+
+}
