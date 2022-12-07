2340dbc1d7825b297d151e855b4b0db367a40479
Mathieu George <mathieu.george@koeos.lan>
2022-12-07 09:07:33 +0100

    debut day7

--- src/aocDay7/Day7.java
+++ src/aocDay7/Day7.java
@@ -7,9 +7,18 @@ import java.nio.file.Paths;
 
 public class Day7 {
 	
+	static void interpreteCommand(String command) {
+		
+	}
+	
 	public static void main(String[] args) throws IOException {
 		Path inputPath = Paths.get("src/aocDay7/input_day7.txt");
         String input = Files.readString(inputPath);
+        
+        String[] commands = input.split("\r\n");
+        for (String command : commands) {
+			
+		}
 	}
 
 }
