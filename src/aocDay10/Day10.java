6ff4855c3d9a02e78fdf15d118e08005323f34d9
mg <mathieu.george92@gmail.com>
2022-12-09 09:38:52 +0100

    day 10 prep

---
+++ src/aocDay10/Day10.java
@@ -0,0 +1,16 @@
+package aocDay10;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Day10 {
+
+    public static void main(String[] args) throws IOException {
+        Path inputPath = Paths.get("src/aocDay10/input_day10.txt");
+        String input = Files.readString(inputPath);
+    }
+
+
+}
