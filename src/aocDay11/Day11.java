03a289da51f11f4ef7bddf3b2925dcbd6f158259
mg <mathieu.george92@gmail.com>
2022-12-11 19:46:00 +0100

    removed imports

--- src/aocDay11/Day11.java
+++ src/aocDay11/Day11.java
@@ -1,8 +1,6 @@
 package aocDay11;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
